221a695753fd93b39c52a8e5a503e6dd10494e41
weselben <bengottwaldi04@gmail.com>
2022-02-10 23:34:57 +0100

    Adding website.description to var replacer

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require "strings.php";
 $content = file_get_contents('assets/html/header.html');
 
 $content = str_replace("%website.keywords%", getString("website.keywords", getLang()), $content);
+$content = str_replace("%website.description%", getString("website.description", getLang()), $content);
 $content = str_replace("%php.language%", getLang(), $content);
 $content = str_replace("%website.title%", getString("website.title", getLang()), $content);
 
