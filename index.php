4f7eba5ff9d897321099f403abe6ff6d47c7d464
weselben <bengottwaldi04@gmail.com>
2022-02-10 22:53:42 +0100

    Changes by Finn

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
+
 include "getlang.php";
+include_once "strings.php";
+
+$content = file_get_contents('assets/html/header.html');
+
+$content = str_replace("%website.keywords%", getString("website.keywords", getLang()), $content);
+$content = str_replace("%php.language%", getLang(), $content);
+$content = str_replace("%website.title%", getString("website.title", getLang()), $content);
+
+
+echo $content;
+
+
 ?>
